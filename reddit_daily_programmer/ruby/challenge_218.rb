edca0d2af65aecb71ebf638336896e482283c060
Angel Perez <iangel.p93@gmail.com>
2016-07-28 20:19:37 -0400

    Added Daily Programmer Challenge 218

---
+++ reddit_daily_programmer/ruby/challenge_218.rb
@@ -0,0 +1,26 @@
+# [2015-06-15] Challenge #218 [Easy] To-do list (Part 1)
+# https://www.reddit.com/r/dailyprogrammer/comments/39ws1x/20150615_challenge_218_easy_todo_list_part_1/
+class ToDoList
+  def initialize
+    @items = []
+  end
+
+  def add_item
+    puts 'Enter your task: '
+    item = gets.chomp
+    @items.push(item)
+  end
+
+  def show_items
+    @items.each_with_index do |v, k|
+      puts "#{k}. #{v}"
+    end
+  end
+
+  def delete_item
+    puts 'Enter the # of the task you want to delete: '
+    item = gets.to_i
+    @items.delete_at(item)
+    puts 'Item deleted'
+  end
+end
